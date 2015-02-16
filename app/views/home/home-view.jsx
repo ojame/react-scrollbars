6dbd37a46c2d786830d3bbdd784c729d1c445fd4
James Coleman <james.coleman@bugherd.com>
2015-02-16 14:26:48 +1100

    padding example

--- app/views/home/home-view.jsx
+++ app/views/home/home-view.jsx
@@ -3,6 +3,7 @@
 var React = require('react');
 var VerticalScrollbar = require('./examples/vertical-scrollbar.jsx');
 var NoVerticalOverflow = require('./examples/no-vertical-scrollbar.jsx');
+var VerticalScrollbarPadding = require('./examples/vertical-scrollbar-padding.jsx');
 
 module.exports = React.createClass({
 
@@ -18,6 +19,9 @@ module.exports = React.createClass({
 
         <h3>Standard</h3>
         <VerticalScrollbar />
+
+        <h3>Content with padding</h3>
+        <VerticalScrollbarPadding />
 
         <h3>No scrollbar</h3>
         <NoVerticalOverflow />
