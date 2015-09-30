7aa89592ff522914a06ad60adc2883bd20eedb76
James Coleman <james.coleman@bugherd.com>
2015-09-30 12:04:19 +1000

    es6ify home

--- src/__examples__/index.js
+++ src/__examples__/index.js
@@ -4,12 +4,11 @@ import Home from './views/home/index';
 require('normalize.css/normalize.css');
 require('./app.css');
 
-var App = React.createClass({
-  render: function () {
+class App extends React.Component {
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <header className="App-header" />
 
         <div className="App-content">
           <Home />
@@ -17,6 +16,6 @@ var App = React.createClass({
       </div>
     );
   }
-});
+};
 
 React.render(<App />, document.body);
